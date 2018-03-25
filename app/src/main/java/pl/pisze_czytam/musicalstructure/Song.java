1ca47abf26e1f61a8a5423b61ae995e4f8933476
Urszula Drabińska <u.drabinska@gmail.com>
2018-03-25 21:47:58 +0200

    Song class added

--- app/src/main/java/pl/pisze_czytam/musicalstructure/Song.java
+++ app/src/main/java/pl/pisze_czytam/musicalstructure/Song.java
@@ -1,4 +1,28 @@
 package pl.pisze_czytam.musicalstructure;
 
 public class Song {
+    private int mAlbumCover;
+    private String mSongTitle;
+    private String mAlbumTitle;
+    private String mArtistName;
+
+    public Song(int albumCover, String songTitle, String albumTitle, String artistName) {
+        mAlbumCover = albumCover;
+        mSongTitle = songTitle;
+        mAlbumTitle = albumTitle;
+        mArtistName = artistName;
+    }
+
+    public int getAlbumCover() {
+        return mAlbumCover;
+    }
+    public String getSongTitle() {
+        return mSongTitle;
+    }
+    public String getAlbumTitle() {
+        return mAlbumTitle;
+    }
+    public String getArtistName() {
+        return mArtistName;
+    }
 }
